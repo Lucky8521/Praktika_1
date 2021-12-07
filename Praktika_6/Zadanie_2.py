f983b2fc8960f57c6d7dbee70b2bba388a0a4add
Lucky8521 <ya.bes1010@yandex.ru>
2021-12-07 13:12:20 +0300

    Create Zadanie_2.py

---
+++ Praktika_6/Zadanie_2.py
@@ -0,0 +1,30 @@
+def spirt():
+    mas = dict()
+    mas_1 = []
+    element = 0
+    l = 0
+    el_C = 2
+    el_H = 6
+    el_O = 1
+    with open('input.txt', 'r') as input_file:
+        for line in input_file:
+            line = line.split(';')
+            for i in line:
+                spl_3 = i[2:]
+                spl_4 = i[0:1]
+                element += 1
+                mas[element] = {"elem":spl_4,"kol":int(spl_3)}
+        for q in mas:
+            l = mas[q]["kol"]
+            if mas[q]["elem"] == "C":              
+                kol_vo = l // el_C
+            if mas[q]["elem"] == "H":
+                kol_vo = l // el_H
+            if mas[q]["elem"] == "O":
+                kol_vo = l // el_O
+            mas_1.append(kol_vo)
+        minimal = min(mas_1, key=lambda i: int(i))
+        print(min(mas_1, key=lambda i: int(i)))
+    output_file = open("Output.txt", "w")
+    output_file.write(str(minimal) + "\n")
+spirt()
