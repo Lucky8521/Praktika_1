a354b17671ccebd941c29d147ead7eb662c2ef8d
Lucky8521 <ya.bes1010@yandex.ru>
2021-12-07 14:51:39 +0300

    Update Zadanie_3.py

--- Praktika_6/Zadanie_3.py
+++ Praktika_6/Zadanie_3.py
@@ -2,32 +2,24 @@ import base64
 # XOR для шифрования/расшифровки
 def xor_cipher( str, key ):
     encript_str = ""
-    for letter in str:
-        encript_str += chr( ord(letter) ^ key )
-    return  encript_str
+    a_list = [chr(ord(a) ^ ord(b)) for a,b in zip(str, key)]
+    return a_list
 chikal = True   
 while chikal: 
     with open('input_2.txt', 'r', encoding='utf-8') as input_file: 
         for line in input_file:
             strg = line
-    try:
-            keyy = int(input("Введите Ключ шифрования: "))
-            if keyy < 0:
-                print("Число не должно быть отрицательным!!!")
-                continue
+            keyy = (input("Введите Ключ шифрования: "))
             print( "Оригинал:\t\t\t", strg )
             encr_strg = xor_cipher( strg, keyy ) 
-            print( "Зашифровонное сообщение:\t", encr_strg )
+            print( "Зашифровонное сообщение:\t", "".join(encr_strg) )
             output_file = open("Output_2.txt", "w", encoding='utf-8')
             output_file.write(str(encr_strg) + "\n")
-            print( "Расшифрованное сообщение:\t", xor_cipher( encr_strg, keyy ) )
-    except ValueError:
-        print("Введите число!!!")
+            print( "Расшифрованное сообщение:\t", "".join(xor_cipher( encr_strg, keyy )))
     while True:
         clode = input("Хотите ли вы выйти? (Да/Нет)")
         if clode == "Да":
             chikal = False
-            break
         elif clode == "Нет": 
             break
         elif clode != "Да" or clode != "Нет":
