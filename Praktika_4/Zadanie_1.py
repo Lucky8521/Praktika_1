a1dfd1f51834add30d2a8312ae7b8cab1617819c
Lucky8521 <ya.bes1010@yandex.ru>
2021-12-11 12:15:36 +0300

    Update Zadanie_1.py

--- Praktika_4/Zadanie_1.py
+++ Praktika_4/Zadanie_1.py
@@ -1,15 +1,14 @@
 from random import randint
 a = 5 
-b = [9, 8, 7 , 6 , 5]
+b = []
+"""Заполнение рандомно числе в список"""
 for i in range(a):
    b.append(randint(1, 20))
 print(b)
-
-for i in range(a - 1):
-    #print (i)
-    for j in range(a-i -1):
+"""Раставление списка в порядке возрастания. Метод пузырька"""
+for i in range(a-1):
+    for j in range(a-i-1):
         if b[j] > b[j+1]:
             b[j], b[j+1] = b[j+1], b[j]
-    print(b)
 print(b)
 
