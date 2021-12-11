13526f95cb96b7ca42891cae76a81bb1ad9ffc30
Lucky8521 <ya.bes1010@yandex.ru>
2021-12-11 12:15:32 +0300

    Update Zadanie_3.py

--- Praktika_3/Zadanie_3.py
+++ Praktika_3/Zadanie_3.py
@@ -1,30 +1,28 @@
 from math import *
 print ("решаем квадратное уравнение: a*x^2 +b*x + c=0")
+"""Цикл для поиска дискриминанта"""
 while True:
     try:
         a =int(input("Введите перменную a: "))
         break
     except ValueError:
             print('Неверный формат, введите целое число!')
-            #continue
 while True:
     try:
         b = int(input("Введите перменную b: "))
         break
     except ValueError:
             print('Неверный формат, введите целое число!')
-            #continue
 while True:
     try:
         c = int(input("Введите перменную c: "))
         break
     except ValueError:
             print('Неверный формат, введите целое число!')
-            #continue
 discriminant = b ** 2 - 4*a*c
 if discriminant > 0:
-    x_1= ((-b) - sqrt(discriminant))/(2*a)
-    x_2= ((-b) + sqrt(discriminant)) / (2*a)
+    x_1= ((-b)-sqrt(discriminant))/(2*a)
+    x_2= ((-b)+sqrt(discriminant))/(2*a)
     print("Два корня: " + "x_1: "+ str(round(x_1, 3)) +"\t"+ "x_2: " + str(round(x_2, 3)))
 elif discriminant == 0:
     x_1 = -b/(2*a)
