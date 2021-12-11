372301a0ce451a29e18b609035436c87052125bf
Lucky8521 <ya.bes1010@yandex.ru>
2021-12-11 12:15:34 +0300

    Update Zadanie_4.py

--- Praktika_3/Zadanie_4.py
+++ Praktika_3/Zadanie_4.py
@@ -1,15 +1,15 @@
-# Решил попробовать с функцией!
 from math import *
 from typing import Counter
 from sympy import *
-#Объявляем функцию
+"""Функция znach() для укарачивание кода, запоминаем все 3 ввода в переменную а"""
 def znach(value_check, flag_a = False):
     while True:
         try:
             #Объявляем переменную
             a =int(input("Введите перменную " + value_check))
             #Проверка на 0 == 0
-            if (flag_a and a == 0):
+            if (flag_a and 
+                a == 0):
                 print("Переменная а не может равняться 0")
             else:
                 return a
@@ -24,8 +24,8 @@ c = znach('c: ')
 #Ищем дискриминант и корни
 discriminant = b ** 2 - 4*a*c
 if discriminant > 0:
-    x_1= ((-b - sqrt(discriminant))/ (2*a))
-    x_2= ((-b + sqrt(discriminant)) / (2*a))
+    x_1= ((-b-sqrt(discriminant))/(2*a))
+    x_2= ((-b+sqrt(discriminant))/(2*a))
     print("Два корня: " + "x_1: "+ str(round(x_1, 3)) +"\t"+ "x_2: " + str(round(x_2, 3)))
 elif discriminant == 0:
     x_1 = -b/(2*a)
